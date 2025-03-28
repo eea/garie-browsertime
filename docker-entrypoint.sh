9ba1d2c14762a8837837a10168749fa3018d2b99
Silviu Panaite <silviu@silviu.edw.ro>
2025-03-28 12:05:21 +0000

    update dependencies

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -6,5 +6,48 @@ if [ -n "$CONFIG" ]; then
 	echo "Found configuration variable, will write it to the /usr/src/garie-plugin/config.json"
 	echo "$CONFIG" > /usr/src/garie-plugin/config.json
 fi
+export DBUS_SESSION_BUS_ADDRESS=/dev/null
 
+# All browsers do not exist in all architectures.
+if [[ `which google-chrome` ]]; then
+   google-chrome --version
+elif [[ `which chromium-browser` ]]; then
+   chromium-browser --version
+fi
+
+if [[ `which firefox` ]]; then
+   firefox --version
+fi
+
+if [[ `which microsoft-edge` ]]; then
+   microsoft-edge --version
+fi
+
+BROWSERTIME_RECORD=/usr/src/app/bin/browsertimeWebPageReplay.js
+BROWSERTIME=/usr/src/app/bin/browsertime.js
+
+CERT_FILE=/webpagereplay/certs/wpr_cert.pem
+KEY_FILE=/webpagereplay/certs/wpr_key.pem
+
+
+WORKDIR_UID=$(stat -c "%u" .)
+WORKDIR_GID=$(stat -c "%g" .)
+
+# Create user with the same UID and GID as the owner of the working directory, which will be used
+# to execute node. This is partly for security and partly so output files won't be owned by root.
+groupadd --non-unique --gid $WORKDIR_GID browsertime
+useradd --non-unique --uid $WORKDIR_UID --gid $WORKDIR_GID --home-dir /tmp browsertime
+
+
+# Here's a hack for fixing the problem with Chrome not starting in time
+# See https://github.com/SeleniumHQ/docker-selenium/issues/87#issuecomment-250475864
+function chromeSetup() {
+  sudo rm -f /var/lib/dbus/machine-id
+  sudo mkdir -p /var/run/dbus
+  sudo service dbus restart > /dev/null
+  service dbus status > /dev/null
+  export $(dbus-launch)
+  export NSS_USE_SHARED_DB=ENABLED
+}
+chromeSetup()
 exec "$@"
--- src/browsertime.sh
+++ src/browsertime.sh
@@ -1,23 +1,29 @@
 #!/usr/bin/env bash
 set -e
 echo "Start getting data"
+REPORTDIR=/usr/src/garie-plugin/reports
+# docker_id=$( cat /proc/self/cgroup | grep :memory: | sed  's#.*/\([0-9a-fA-F]*\)$#\1#' )
 
-docker_id=$( cat /proc/self/cgroup | grep :memory: | sed  's#.*/\([0-9a-fA-F]*\)$#\1#' )
+# REPORTDIR=$(docker inspect $docker_id | grep :/usr/src/garie-plugin/reports | awk -F'["|:]' '{print $2}')
 
-REPORTDIR=$(docker inspect $docker_id | grep :/usr/src/garie-plugin/reports | awk -F'["|:]' '{print $2}')
+# if [[ $2 =~ "on-demand" ]]
+# then
+#     REPORTDIR="$REPORTDIR/on-demand/"
+# fi
 
-if [[ $2 =~ "on-demand" ]]
-then
-    REPORTDIR="$REPORTDIR/on-demand/"
-fi
-
-mkdir -p $REPORTDIR
+# mkdir -p $REPORTDIR
 
 echo "Saving reports into $REPORTDIR"
 
 echo "Getting data for: $1"
+cd $REPORTDIR
+hash=$(echo -n "$1" | md5sum | awk '{print $1}')  # Get the MD5 hash of the URL
+unique_int=$((0x${hash:0:8}))  # Convert the first 8 characters of the hash (hex) to a decimal integer
 
-timeout 1800 docker run --rm --shm-size=1g --cpus=$3 -v $REPORTDIR:/browsertime sitespeedio/browsertime $1
+# Map the unique integer to a display number and remote debugging port
+display_number=$((1000 + unique_int % 1000))  # Map the hash to a display number (between 1000-1999)
+
+/usr/src/app/bin/browsertime.js --xvfbParams.display $display_number $1
 
 echo "Finished getting data for: $1"
 
